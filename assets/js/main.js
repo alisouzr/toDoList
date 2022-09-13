27fd1284e90d69807a999164b0001529a1e75231
Aliny <aliny.souz.r@gmail.com>
2022-09-13 16:32:30 -0300

    Adicionando comentarios ao codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,16 @@
+//criação de constantes
 const adicionar = document.querySelector("#add");
 const apagar = document.querySelector("#remove");
 const finalizada = document.querySelector("#finish");
 const divTarefas = document.querySelector(".tarefas");
 const tarefa = document.querySelector("#tarefas_input");
 const select = document.querySelector("#select");
+
+//variaveis que serão usadas em funções diferentes
 let div;
 let contador = 0;
 
+//funçao para ficar ouvindo se o usuario deu um enter no teclado, caso sim o botao adicionar será pressionado add um novo item
 tarefa.addEventListener('keypress', function (e) {
     if (e.keyCode === 13) {
         adicionar.click()
@@ -14,12 +18,14 @@ tarefa.addEventListener('keypress', function (e) {
     }
 })
 
+//botao que seleciona todos os itens da lista de uma só vez
 select.addEventListener('click', () => {
     document.querySelectorAll('input[name="itemCheck"]').forEach(function (e) {
         e.checked = true;
     });
 })
 
+//botao para adiconar a task que o usuario digitou no inputText
 adicionar.addEventListener('click', () => {
     if (!tarefa.value) return;
 
@@ -36,6 +42,7 @@ adicionar.addEventListener('click', () => {
     localStorage.setItem(input.id, div.outerHTML)
 })
 
+//cria um inputCheck para cada item que o usuario adiciona
 function createInput() {
     const input = document.createElement('input');
     input.setAttribute("type", "checkbox");
@@ -44,6 +51,7 @@ function createInput() {
     return input;
 }
 
+//cria uma label pro inputCheck a cada item adicionado pelo usuario
 function createLabel() {
     const label = document.createElement('label');
     label.setAttribute("for", `id_input${contador}`);
@@ -51,6 +59,7 @@ function createLabel() {
     return label
 }
 
+//botão que apaga o item que o usuario marcou como check
 apagar.addEventListener('click', () => {
     document.querySelectorAll('input[name="itemCheck"]').forEach(function (e) {
         let idInput = e.id;
@@ -63,19 +72,21 @@ apagar.addEventListener('click', () => {
     });
 })
 
-
+//salva no localStorage o item que o usuario marcou como check
 function salvarChecked(label, atributoForLabel) {
     let input = document.querySelector('#' + atributoForLabel);
     input.setAttribute('checked', 'true')
     localStorage.setItem(input.id, label.outerHTML)
 }
 
+//função que retira do localStorage caso o usuario tenha tirado o check do item
 function semChecked(label, atributoForLabel) {
     let input = document.querySelector('#' + atributoForLabel);
     input.removeAttribute('checked')
     localStorage.setItem(input.id, label.outerHTML)
 }
 
+//atualiza os dados, caso no localStorage tenha algum dado ele adiciona na tela
 window.addEventListener('load', () => {
     let cont = localStorage.getItem("contador");
     contador = cont
@@ -93,6 +104,7 @@ window.addEventListener('load', () => {
     }
 })
 
+//risca o item da lista caso o usuario tenha selecionado o item
 window.addEventListener('click', () => {
     document.querySelectorAll("label").forEach(function (e) {
         let atributoForLabel = e.getAttribute("for");
