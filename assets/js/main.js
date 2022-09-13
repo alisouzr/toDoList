fd0112bd885ed46056f6cdb32333308ba7e9881b
Aliny <aliny.souz.r@gmail.com>
2022-09-13 16:05:55 -0300

    Função createLabel e createInput adicionadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ const finalizada = document.querySelector("#finish");
 const divTarefas = document.querySelector(".tarefas");
 const tarefa = document.querySelector("#tarefas_input");
 const select = document.querySelector("#select");
-let numero = 0;
 let div;
 let contador = 0;
 
@@ -26,22 +25,31 @@ adicionar.addEventListener('click', () => {
 
     contador++
     localStorage.setItem("contador", contador);
-    const label = document.createElement('label');
-    const input = document.createElement('input');
+    label = createLabel()
+    input = createInput()
     div = document.createElement('div');
-    input.setAttribute("type", "checkbox");
-    input.setAttribute("name", "itemCheck");
-    input.setAttribute("id", `id_input${contador}`);
-    label.setAttribute("for", `id_input${contador}`);
-    label.textContent = tarefa.value
     div.appendChild(input)
     div.appendChild(label)
     divTarefas.appendChild(div);
 
-
     tarefa.value = ""
     localStorage.setItem(input.id, div.outerHTML)
 })
+
+function createInput() {
+    const input = document.createElement('input');
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("name", "itemCheck");
+    input.setAttribute("id", `id_input${contador}`);
+    return input;
+}
+
+function createLabel() {
+    const label = document.createElement('label');
+    label.setAttribute("for", `id_input${contador}`);
+    label.textContent = tarefa.value
+    return label
+}
 
 apagar.addEventListener('click', () => {
     document.querySelectorAll('input[name="itemCheck"]').forEach(function (e) {
@@ -55,18 +63,6 @@ apagar.addEventListener('click', () => {
     });
 })
 
-window.addEventListener('click', () => {
-    document.querySelectorAll("label").forEach(function (e) {
-        let atributoForLabel = e.getAttribute("for");
-        if (document.querySelector("#" + atributoForLabel).checked == true) {
-            e.style.textDecoration = "line-through";
-            salvarChecked(e.parentNode, atributoForLabel);
-        } else {
-            e.style.textDecoration = "none"
-            semChecked(e.parentNode, atributoForLabel)
-        }
-    })
-})
 
 function salvarChecked(label, atributoForLabel) {
     let input = document.querySelector('#' + atributoForLabel);
@@ -95,4 +91,17 @@ window.addEventListener('load', () => {
             }
         }
     }
+})
+
+window.addEventListener('click', () => {
+    document.querySelectorAll("label").forEach(function (e) {
+        let atributoForLabel = e.getAttribute("for");
+        if (document.querySelector("#" + atributoForLabel).checked == true) {
+            e.style.textDecoration = "line-through";
+            salvarChecked(e.parentNode, atributoForLabel);
+        } else {
+            e.style.textDecoration = "none"
+            semChecked(e.parentNode, atributoForLabel)
+        }
+    })
 })
